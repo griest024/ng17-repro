f5dcffe82e2c127f9d1fe006840bc836a51ab950
Peter Lauck <griest024@gmail.com>
2024-01-31 00:22:54 +0000

    works when child is rendered without router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { RouterModule } from "@angular/router";
 import { routes } from "./app.routes";
 import { AppComponent } from "./app.component";
 import { BrowserModule, provideClientHydration } from "@angular/platform-browser";
+import { ChildComponent } from "./child/child.component";
 
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ChildComponent
   ],
   bootstrap: [AppComponent],
   declarations: [AppComponent],
